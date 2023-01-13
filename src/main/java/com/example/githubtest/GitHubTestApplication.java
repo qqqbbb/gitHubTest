80ac3cb7a1ecf485cd6ed72060817571be8a1635
qqqbbb <qqqinbox@ya.ru>
2023-01-13 13:13:11 +0300

    First commit

---
+++ src/main/java/com/example/githubtest/GitHubTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.githubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubTestApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GitHubTestApplication.class, args);
+    }
+
+}
